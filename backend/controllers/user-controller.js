5acd51ad3d0b61af4f6592da63c09f5e4ce63380
Pavel <artpavel@ukr.net>
2022-07-12 13:34:36 +0300

    finish create mehtod auth and login and token. It's was backend

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user-models');
 
@@ -38,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -49,10 +51,44 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route');
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  // check user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+// Generate token
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Protected
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+  res.status(200).json(user);
 });
 
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 // out files
-const { registerUser, loginUser } = require('../controllers/user-controller');
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/user-controller');
+const { protect } = require('../midlleware/auth-middleware');
 
 router.post('/', registerUser);
 
-// /api/users/login
+// ~/api/users/login
 router.post('/login', loginUser);
+
+// protected ~/api/users/me
+router.get('/me', protect, getMe);
 
 module.exports = router;
